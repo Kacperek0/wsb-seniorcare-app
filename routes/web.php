1c2cd6093a7a23e7f7791764dab220424f9e84c9
Kacper Szczepanek <kacper.szczepanek@nordcloud.com>
2021-12-23 00:13:34 +0100

    add email verification

--- routes/web.php
+++ routes/web.php
@@ -21,11 +21,9 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Auth::routes();
+Auth::routes(['verify' => true]);
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-
-Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
