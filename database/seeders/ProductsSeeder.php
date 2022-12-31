e7ccca0731f339247aa7a9a8a72bf38e9081b048
Kacperek0 <47487512+Kacperek0@users.noreply.github.com>
2022-12-31 16:58:40 +0100

    fixtures (#19)

--- database/seeders/ProductsSeeder.php
+++ database/seeders/ProductsSeeder.php
@@ -16,11 +16,11 @@ class ProductsSeeder extends Seeder
     public function run()
     {
         $products = [
-            ['Butter', 'Regular butter', '6.50'],
-            ['Bread', 'Just a bread', '2.99'],
-            ['Lech Pils', 'Beer from local brewery', '2.59'],
-            ['Pepsi 2l', 'Cuz its better than Cola', '8.29'],
-            ['Roll', 'A regular roll', '0.39']
+            ['Butter', 'Regular butter', '9.50'],
+            ['Bread', 'Just a bread', '3.99'],
+            ['Lech Pils', 'Beer from local brewery', '3.59'],
+            ['Pepsi 2l', 'Cuz its better than Cola', '9.29'],
+            ['Roll', 'A regular roll', '0.59']
         ];
 
         foreach ($products as $item) {
